899b4f81b8a1303e3acb6b03a2673abc33cd8d93
Franck Thang <stelyus@outlook.com>
2018-07-04 17:38:01 +0200

    location.asvc -> location.avsc + most of model checked and approved by Franck

--- src/main/scala/com/sn/spark/core/Main.scala
+++ src/main/scala/com/sn/spark/core/Main.scala
@@ -1,9 +1,9 @@
 import java.time.Instant
 
-import com.sn.spark.core.model.{Id, Message, Post, User}
+import com.sn.spark.core.model._
 import org.apache.kafka.clients.producer.KafkaProducer
-import com.sn.spark.core.producer.{MessageProducer, PostProducer}
-import com.sn.spark.core.consumer.{MessageConsumer, PostConsumer}
+import com.sn.spark.core.producer.{LikeProducer, LocationProducer, MessageProducer, PostProducer}
+import com.sn.spark.core.consumer.{LikeConsumer, LocationConsumer, MessageConsumer, PostConsumer}
 import org.apache.log4j.BasicConfigurator
 
 import scala.util.Random
@@ -12,8 +12,10 @@ object Main extends App {
   override def main(args: Array[String]): Unit = {
     BasicConfigurator.configure()
 
-    sendPost()
-    sendMessage()
+//    sendPost()
+//    sendMessage()
+//    sendLike()
+//    sendLocation()
   }
 
 
@@ -115,4 +117,98 @@ object Main extends App {
 
     postProducer.close()
   }
+
+  def sendLike(): Unit = {
+    val likeConsumer = LikeConsumer.createConsumer()
+    val likesTopic: String = "like-topic"
+
+    LikeConsumer.read(likesTopic, likeConsumer)
+    val likeProducer: KafkaProducer[String, Array[Byte]] = LikeProducer.createProducer()
+    var i: Int = 0
+    // Send 100 Post on Topic posts-topic
+
+
+    val arrayUser: List[String] = List("Gabriel", "Adam", "Raphael", "Paul", "Louis", "Arthur", "Alexandre", "Victor",
+      "Jules", "Mohamed", "Lucas", "Joseph", "Antoine", "Gaspard", "Maxime")
+    val usersLength: Int = arrayUser.length
+    val r: Random = scala.util.Random
+
+    while (i < 100) {
+      LikeProducer.send[Like](
+        likesTopic,
+        Like(
+          Id[Like]("like"+i),
+          Id[User](arrayUser(r.nextInt(usersLength))),
+          Instant.now(),
+          Id[Post]("post"+i)
+        ),
+        likeProducer
+      )
+
+
+      Thread.sleep(2000)
+      i += 1
+    }
+
+    likeProducer.close()
+  }
+
+  def sendLocation(): Unit = {
+    val locConsumer = LocationConsumer.createConsumer()
+    val locTopic: String = "location-topic"
+
+    LocationConsumer.read(locTopic, locConsumer)
+    val locProducer: KafkaProducer[String, Array[Byte]] = LocationProducer.createProducer()
+    var i: Int = 0
+    // Send 100 Post on Topic posts-topic
+
+
+    val arrayUser: List[String] = List("Gabriel", "Adam", "Raphael", "Paul", "Louis", "Arthur", "Alexandre", "Victor",
+      "Jules", "Mohamed", "Lucas", "Joseph", "Antoine", "Gaspard", "Maxime")
+    val usersLength: Int = arrayUser.length
+
+    val cityCountryMap = Map(
+      "Paris" -> "France",
+      "Nice" -> "France",
+      "Marseille" -> "France",
+      "Tokyo" -> "Japon",
+      "New York" -> "USA",
+      "Saint-Barthelemy" -> "France",
+      "Shanghai" -> "Chine",
+      "Seoul" -> "Coree du Sud",
+      "Berlin" -> "Allemagne",
+      "Barcelone" -> "Espagne",
+      "Madrid" -> "Espagne",
+      "Taipei" -> "Taiwan",
+      "Moscou" -> "Russie"
+    )
+    val cityMapSize = cityCountryMap.size
+
+    val r: Random = scala.util.Random
+
+    while (i < 100) {
+      val city: String = cityCountryMap.keys.toSeq(r.nextInt(cityMapSize))
+      val country: String = cityCountryMap.getOrElse(city, "Undefined")
+
+
+      LocationProducer.send[Location](
+        locTopic,
+        Location(
+          Id[Location]("location"+i),
+          Instant.now(),
+          Id[User](arrayUser(r.nextInt(usersLength))),
+          city,
+          country
+        ),
+        locProducer
+      )
+
+
+      Thread.sleep(2000)
+      i += 1
+    }
+
+    locProducer.close()
+  }
+
 }
